0ebed00483255417d8327b76b090f0ec5c2bcff8
trinhquocdat041004@gmail.com <trinhquocdat041004@gmail.com>
2025-02-24 13:34:01 +0700

    init

---
+++ TestUnit/src/main/java/com/tqd/testunit/TestUnit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tqd.testunit;
+
+/**
+ *
+ * @author admin
+ */
+public class TestUnit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
