3ddd40ab2e9a39843ce2bf65f61e9522c21a4b78
trinhquocdat041004@gmail.com <trinhquocdat041004@gmail.com>
2025-02-24 14:08:26 +0700

    update coder

--- TestUnit/src/main/java/com/tqd/testunit/TestUnit.java
+++ TestUnit/src/main/java/com/tqd/testunit/TestUnit.java
@@ -12,6 +12,6 @@ public class TestUnit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.print("Hello");
+        System.out.print("Hello 01 banch ");
     }
 }
